7e08ce21e2f35854ab63312cc58433793552ee79
ruby-haigh <rubyimogen.h@gmail.com>
2026-03-10 15:50:43 +0000

    CRUD with tests for create and delete

--- src/main/java/com/makersacademy/acebook/model/Post.java
+++ src/main/java/com/makersacademy/acebook/model/Post.java
@@ -3,9 +3,14 @@ package com.makersacademy.acebook.model;
 import jakarta.persistence.*;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Data
 @Entity
+@Getter @Setter
 @Table(name = "POSTS")
 public class Post {
 
@@ -13,6 +18,7 @@ public class Post {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String content;
+    private LocalDateTime updatedAt;
 
     public Post() {}
 
