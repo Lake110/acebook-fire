947fa590d1319c55b17175168088295c74c5e843
Michael Lake <mlake0911@gmail.com>
2026-03-10 10:41:13 +0000

    add homefeed.html and rework HomeController

--- src/main/java/com/makersacademy/acebook/controller/HomeController.java
+++ src/main/java/com/makersacademy/acebook/controller/HomeController.java
@@ -1,13 +1,28 @@
 package com.makersacademy.acebook.controller;
 
+import com.makersacademy.acebook.model.Post;
+import com.makersacademy.acebook.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
-	@RequestMapping(value = "/")
-	public RedirectView index() {
-		return new RedirectView("/posts");
+
+	@Autowired
+	private PostRepository postRepository;
+
+	@GetMapping("/")
+	public ModelAndView IndexPage() {
+		ModelAndView modelAndView = new ModelAndView("posts/homefeed");
+		List<Post> posts = postRepository.findAll();
+		modelAndView.addObject("posts", posts);
+		return modelAndView;
 	}
 }
+
--- src/main/java/com/makersacademy/acebook/controller/PostsController.java
+++ src/main/java/com/makersacademy/acebook/controller/PostsController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
@@ -21,12 +22,13 @@ public class PostsController {
         Iterable<Post> posts = repository.findAll();
         model.addAttribute("posts", posts);
         model.addAttribute("post", new Post());
-        return "posts/index";
+        return "posts/posts";
     }
 
     @PostMapping("/posts")
-    public RedirectView create(@ModelAttribute Post post) {
+    public RedirectView create(@ModelAttribute Post post, RedirectAttributes redirectAttributes) {
         repository.save(post);
+        redirectAttributes.addFlashAttribute("message", "Post submitted!");
         return new RedirectView("/posts");
     }
 }
