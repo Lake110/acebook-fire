e10aedb577f7530e61e1dddc1c884ff4d883f43a
Michael Lake <mlake0911@gmail.com>
2026-03-10 15:59:34 +0000

    add user settings page with CRUD

--- src/main/java/com/makersacademy/acebook/controller/UsersController.java
+++ src/main/java/com/makersacademy/acebook/controller/UsersController.java
@@ -1,17 +1,25 @@
 package com.makersacademy.acebook.controller;
 
 import com.makersacademy.acebook.model.User;
+import com.makersacademy.acebook.repository.PostRepository;
 import com.makersacademy.acebook.repository.UserRepository;
+import jakarta.servlet.http.HttpSession;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
-@RestController
+@Controller
 public class UsersController {
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    PostRepository postRepository;
 
     @GetMapping("/users/after-login")
     public RedirectView afterLogin() {
@@ -31,5 +39,52 @@ public class UsersController {
         });
 
         return new RedirectView("/posts");
+    }
+
+    @GetMapping("/settings")
+    public ModelAndView settings() {
+        DefaultOidcUser principal = (DefaultOidcUser) SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getPrincipal();
+        String email = (String) principal.getAttributes().get("email");
+
+        User user = userRepository.findUserByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        ModelAndView modelAndView = new ModelAndView("/users/settings");
+        modelAndView.addObject("user", user);
+        return modelAndView;
+    }
+
+    @PostMapping("/users/settings/username")
+    public RedirectView updateUsername(@RequestParam String username) {
+        DefaultOidcUser principal = (DefaultOidcUser) SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getPrincipal();
+        String email = (String) principal.getAttributes().get("email");
+
+        userRepository.findUserByEmail(email).ifPresent(user -> {
+            user.setUsername(username);
+            userRepository.save(user);
+        });
+
+        return new RedirectView("/settings");
+    }
+    @Transactional
+    @PostMapping("/users/settings/delete")
+    public RedirectView deleteAccount(HttpSession session) {
+        DefaultOidcUser principal = (DefaultOidcUser) SecurityContextHolder
+                .getContext().getAuthentication().getPrincipal();
+        String email = (String) principal.getAttributes().get("email");
+
+        userRepository.findUserByEmail(email).ifPresent(user -> {
+            postRepository.deleteAllByUser(user);
+            userRepository.delete(user);
+        });
+
+        session.invalidate();
+        return new RedirectView("/");
     }
 }
--- src/main/java/com/makersacademy/acebook/repository/PostRepository.java
+++ src/main/java/com/makersacademy/acebook/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package com.makersacademy.acebook.repository;
 
 import com.makersacademy.acebook.model.Post;
+import com.makersacademy.acebook.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
 
@@ -8,4 +9,5 @@ import java.util.List;
 
 public interface PostRepository extends CrudRepository<Post, Long> {
     List<Post> findAllByOrderByCreatedAtDesc();
+    void deleteAllByUser(User user);
 }
