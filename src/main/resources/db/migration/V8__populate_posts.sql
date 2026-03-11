b0689001f26081c89ae970f12e5a8cc8d424a55a
Safia Omar <safia101@outlook.com>
2026-03-11 15:12:20 +0000

    added migration to populate posts

---
+++ src/main/resources/db/migration/V8__populate_posts.sql
@@ -0,0 +1,6 @@
+INSERT INTO posts (content) VALUES ('Hello Acebook!');
+INSERT INTO posts (content) VALUES ('My first post');
+INSERT INTO posts (content) VALUES ('Testing the feed');
+INSERT INTO posts (content) VALUES ('Had a great day today');
+INSERT INTO posts (content) VALUES ('Excited to be using Acebook');
+INSERT INTO posts (content) VALUES ('Hey guys!');
