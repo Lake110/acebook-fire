89e25d10db1066963fbe89f846bc9b8ab3faa54b
Michael Lake <mlake0911@gmail.com>
2026-03-12 11:47:39 +0000

    update comments table to allow updated to null

--- src/main/resources/db/migration/V9__add_avatar_to_users_and_update_posts_so_updated_at_can_be_null.sql
+++ src/main/resources/db/migration/V9__add_avatar_to_users_and_update_posts_so_updated_at_can_be_null.sql
@@ -6,3 +6,9 @@ ALTER TABLE posts
     ALTER COLUMN updated_at DROP NOT NULL;
 
 UPDATE posts SET updated_at = NULL;
+
+ALTER TABLE comments
+    ALTER COLUMN updated_at DROP DEFAULT,
+ALTER COLUMN updated_at DROP NOT NULL;
+
+UPDATE comments SET updated_at = NULL;
