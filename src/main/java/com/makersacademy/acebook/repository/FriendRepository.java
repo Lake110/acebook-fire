c199b6db7f8b25d6f4da494b6c532ca519b88ddd
ruby-haigh <rubyimogen.h@gmail.com>
2026-03-12 15:42:46 +0000

    friend count

--- src/main/java/com/makersacademy/acebook/repository/FriendRepository.java
+++ src/main/java/com/makersacademy/acebook/repository/FriendRepository.java
@@ -2,6 +2,8 @@ package com.makersacademy.acebook.repository;
 
 import com.makersacademy.acebook.model.Friend;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface FriendRepository extends JpaRepository<Friend, Long> {
     boolean existsByUserIdAndFriendId(Long userId, Long friendId);
@@ -10,4 +12,10 @@ public interface FriendRepository extends JpaRepository<Friend, Long> {
         Long higher = Math.max(userId, friendId);
         return existsByUserIdAndFriendId(lower, higher);
     }
+    @Query("""
+    SELECT COUNT(f)
+    FROM Friend f
+    WHERE f.userId = :userId OR f.friendId = :userId
+""")
+    long countFriends(@Param("userId") Long userId);
 }
